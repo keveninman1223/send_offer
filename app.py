9bb2e3f388d4af5741baa10a8852121c19a9157a
Keven Inman <keven.inman@gmail.com>
2025-10-22 11:20:48 -0700

    Add reply-to for team group email

--- app.py
+++ app.py
@@ -131,6 +131,7 @@ def send_email(
             "to": [seller_email],
             "subject": f"Offer for Your Property at {property_address}",
             "html": html_content,
+            "reply_to": ["offers@capitalreigroup.com"],
             "attachments": [
                 {"filename": "Preliminary_Offer.pdf", "content": list(pdf_content)}
             ],
